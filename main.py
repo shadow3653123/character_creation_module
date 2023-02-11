cec46e56c479337a7dd0a7a6c81f3165074ac5a2
Влад Болтушенко <boltuschenko@gmail.com>
2023-02-11 13:29:49 +0300

    добавление анимаций

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from random import randint
 
-from Graphic_arts.start_game_banner import run_screensaver
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
